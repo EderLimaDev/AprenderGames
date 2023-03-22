e037080aa31fb52f96d740a41b17fb5cc0b8049e
EderLimaDev <ederlimarh@gmail.com>
2023-03-21 23:44:38 -0300

    Footer Implement, and nav button bug corrections

---
+++ script/index.js
@@ -0,0 +1,8 @@
+const kidsImage = document.querySelector("#back-img");
+const buttonNav = document.querySelector(".navbar-toggler");
+
+buttonNav.addEventListener("click", () => {
+    kidsImage.document.toogle.add(".hidden");
+
+      
+})
