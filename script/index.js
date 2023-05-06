6ebd9276239aeb403c9cea8ac6c918be84fc6405
EderLimaDev <ederlimarh@gmail.com>
2023-05-05 21:47:51 -0300

    scroll to top btn e back arrow

--- script/index.js
+++ script/index.js
@@ -7,4 +7,16 @@ buttonNav.addEventListener("click", () => {
 })
 
 
+const scrollBtn = document.querySelector(".scroll-btn");
 
+scrollBtn.addEventListener("click", () =>{
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+
+
+});
+
+window.addEventListener("scroll", function(){
+    scrollBtn.classList.toggle("active", window.scrollY > 500)
+
+})
